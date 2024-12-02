3008b5a1373495995787c7a2a1f7e496dc66e321
Ujjwal1998 <mail2ujjwal98@gmail.com>
2024-12-02 17:42:35 +0530

    1/31 added animation

--- challenge1/vite-project/src/App.tsx
+++ challenge1/vite-project/src/App.tsx
@@ -1,47 +1,36 @@
 import React, { useRef, useState } from "react";
-import lockIcon from "./assets/lock.svg";
-import eyeIcon from "./assets/eye.svg";
+import { Lock } from "./components/lock";
+import { Eyeball } from "./components/Eyeball";
 import "./App.css";
 function App() {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [isHidden, setIsHidden] = useState(false);
+  const [isReveal, setIsReveal] = useState(false);
   function handleReveal() {
     if (inputRef.current) {
       if (inputRef.current.type === "text") {
         inputRef.current.type = "password";
-        setIsHidden(true);
       } else {
         inputRef.current.type = "text";
-        setIsHidden(false);
       }
+      setIsReveal(() => !isReveal);
     }
   }
 
   return (
-    <>
+    <div className="App">
       <h1>Password Input</h1>
-      <div className={`input ${isHidden ? "" : "revealed"}`}>
-        <div className="lock-icon">
-          <img src={lockIcon} alt="" className="lock-icon" />
-        </div>
-        <div className="divider"></div>
-        <div className="password">
-          <input
-            type="text"
-            className={`password-input ${isHidden ? "" : "revealed"}`}
-            ref={inputRef}
-          />
-        </div>
-        <div className="reveal" onClick={handleReveal}>
-          <img
-            src={eyeIcon}
-            alt=""
-            className="eye-icon"
-            // onClick={handleReveal}
-          />
-        </div>
+      <div className={`container ${isReveal ? "revealed" : ""}`}>
+        <Lock></Lock>
+        <input
+          type={`${!isReveal ? "password" : "text"}`}
+          className={`password-input`}
+          ref={inputRef}
+        />
+        <button className="reveal" onClick={handleReveal}>
+          <Eyeball></Eyeball>
+        </button>
       </div>
-    </>
+    </div>
   );
 }
 
---
+++ challenge1/vite-project/src/components/Eyeball.tsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+function Eyeball() {
+  return (
+    <svg
+      className="eyeball"
+      width="110"
+      height="107"
+      viewBox="0 0 110 107"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M105 71C105 71 82.6142 102 55 102C27.3858 102 5 71 5 71"
+        stroke="#26C9C3"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
+      <path d="M70 70C70 78.2843 63.2843 85 55 85C46.7157 85 40 78.2843 40 70C40 61.7157 46.7157 55 55 55C63.2843 55 70 61.7157 70 70Z" />
+      <path
+        d="M105 70C105 70 82.6142 39 55 39C27.3858 39 5 70 5 70"
+        stroke="#26C9C3"
+        strokeWidth="10"
+        strokeLinecap="round"
+      />
+      <path
+        d="M54.9999 22.5V5.5"
+        stroke="#26C9C3"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M91 34.9707L100.686 21"
+        stroke="#26C9C3"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M9.00049 20.9996L18.6856 34.971"
+        stroke="#26C9C3"
+        strokeWidth="10"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export { Eyeball };
